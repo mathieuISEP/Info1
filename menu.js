792b3675847d6ccd38a0c546d45b10c7bee75a3d
antoine jeannette <antoine jeannette>
2016-12-08 16:38:14 +0100

    colors and form in menu + new file menu.js

---
+++ menu.js
@@ -0,0 +1,18 @@
+function opentab(Name) {
+              var i;
+              var x = document.getElementsByClassName("tabcontent");
+              for (i = 0; i < x.length; i++) {
+                  x[i].style.display = "none";
+                  x[i].style.background = "none";
+              }
+              document.getElementById(Name).style.display = "block";
+          }
+          function opensubtab(Name){
+              var i;
+              var x = document.getElementsByClassName("subtab");
+              for (i = 0; i < x.length; i++) {
+                  x[i].style.display = "none";
+                  x[i].style.background = "none";
+              }
+              document.getElementById(Name).style.display = "block";
+          }
