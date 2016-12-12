1f30988f70e716967ce8a2907cab645ea4eee408
antoine jeannette <antoine jeannette>
2016-12-12 13:33:51 +0100

    Checkbox qui marche à moitié!

---
+++ checkbox.js
@@ -0,0 +1,14 @@
+ /*var x=1;
+ function displayifchecked(){
+	if (x%2===0) {
+		document.getElementById("rooms").style.display="none";
+	}
+	else{
+    	document.getElementById("rooms").style.display="block";	
+    }
+    x++;
+    }*/
+
+        function displayifchecked(){
+          document.getElementById("rooms").style.display="block";
+    }
