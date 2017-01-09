c00a841ed7abfd0e84f2b42302794e0e4ac84e30
mathieu <m9.dasilva@gmail.com>
2017-01-09 17:00:01 +0100

    Final temperaturebox TODO INCLUDE PLEASe

--- temperaturebox.php
+++ temperaturebox.php
@@ -15,7 +15,15 @@
      <?php
 
     include 'database.php';
-    $post_temp = $_POST['post_temperature'];
+
+      if (isset($_POST['post_temperature'])) {
+      $post_temp = $_POST['post_temperature'];
+      }
+      else{
+      $post_temp = 20;
+      }
+      
+    
     $sql = "UPDATE sensor SET target_data = $post_temp WHERE id='1'";
     $result = $db -> query($sql);
    
@@ -36,11 +44,21 @@
       <tr>
         <td><div id="housetemp">House Temperature</div></td>
         <td><input value = "<?php $post_temp ?>" name = "post_temperature" placeholder="SET T°" id="stepper" type="number" min="0" max= "30" step="0.5" pattern="[0-9]*"></td>
-        <td class="C">°C</td>
+        <!--<td class="C">°C</td> !-->
       </tr>
       <tr>
-           <td class="target" id="target"><?php echo 'target T°: '. $post_temp;?> </td>
-      </tr>
+      
+           <td class="target" id="target"><?php echo 'target T°: '. $post_temp. ' °C';?> </td>
+            <td class="current" id="current"> 
+
+              <?php $current = "SELECT current_data FROM sensor WHERE id = '1'";
+              $current_result =  $db -> query($current);
+              while ($row = $current_result->fetch_assoc()) {
+              echo 'Current T°: '.$row['current_data']. ' °C'."<br>";
+              }
+            ?> 
+            </td>
+          </tr>
 
 
  
