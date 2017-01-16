c3e9a34c412f6eb11039b772f8bbb7452c4ae1aa
antoine jeannette <antoine jeannette>
2017-01-16 16:40:19 +0100

    edit dashboard

---
+++ editdashboard.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+      <title>Temperature</title>
+        <link href="editdashboard.css" type="text/css" rel="stylesheet">
+        <script type="text/javascript" src="editdashboard.js"></script>
+
+  </head>
+<body>
+<ul>
+<li class="featurecontent">
+<div id="featurecontent">Features
+<ul id="featurecontentlist" align="center" cellpadding="10" cellspacing="10">
+<li><div id="featureelement1" class="featureelement"><span>Temperature</span><span class="arrow">&rarr;</span></div></li>
+<li><div id="featureelement2" class="featureelement"><span>Alarm</spa ><span class="arrow">&rarr;</span></span></li>
+<li><div id="featureelement3" class="featureelement"><span>Shutters</span><span class="arrow">&rarr;</span></div></li>
+<li><div id="featureelement4" class="featureelement"><span>Humidity</span><span class="arrow">&rarr;</span></div></li>
+</ul>
+</div>
+</li>
+</ul>
+<ul>
+<li class="dashboardcontent">
+<div id="dashboardcontent">Dashboard
+<ul id="dashboardcontentlist">
+<li><div id="dashboardelement1" class="dashboardelement"><span class="arrow">&larr;</span><span>Temperature</span></div></li>
+<li><div id="dashboardelement2" class="dashboardelement"><span class="arrow">&larr;</span><span>Alarm </span></div></li>
+<li><div id="dashboardelement3" class="dashboardelement"><span class="arrow">&larr;</span><span>Shutters</span></div></li>
+<li><div id="dashboardelement4" class="dashboardelement"><span class="arrow">&larr;</span><span>Humidity</span></div></li>
+</ul>
+</div>
+</li>
+</ul>	
+</body>
+</html>
