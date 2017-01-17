1764280cf4ff0a2059a7e4f909a5aea8843b2c4c
Leandro <leandrogasparini95@gmail.com>
2017-01-17 16:56:48 +0100

    Working on test.php

--- test.php
+++ test.php
@@ -14,23 +14,32 @@
 
 
 
-    <div id="Rooms" class="tabcontent"> <!-- ____________________________________________ -->
-      <?php
+    <div id="Rooms" > <!-- ____________________________________________ -->
+     <?php
 
-      include ("database.php");
+     include ("database.php");
+
 
       $list_rooms = "SELECT Name_room 
               FROM room 
               WHERE id_home = 1"; 
+
       $results = mysqli_query($db,$list_rooms);
+
       echo '<ul class="navbar2">';
       if ($result=mysqli_query($db,$list_rooms))
         {
         // Fetch one and one row
         while ($row=mysqli_fetch_row($result))
           {
-          echo '<ul class="subtablinks" onclick="opensubtab(';echo $row[0]; echo')">'; 
+          	$table = $dom->createElement('div');
+			$domAttribute = $dom->createAttribute('id');
+			$domAttribute->value = $nb;
 
+			$domAttribute2 = $dom->createAttribute('class');
+			$domAttribute2->value ='subtablinks' ;
+
+         	echo '<ul onclick="opensubtab('; echo $row[0]) ; echo ')" class="subtablinks">';  
           printf ($row[0]);
           echo "</ul>";  
 
@@ -40,35 +49,78 @@
       }
       echo "</ul>";  
 
+  
+     
+
+     $nb = 1;
+     while ( $nb < 9) { 
+
+     	$list_sensors = "SELECT sensor_name 
+              		FROM sensor 
+              		WHERE id_room = $nb"; 
+     	$results2 = mysqli_query($db,$list_sensors); 
+
+		if ($result=mysqli_query($db,$list_sensors))
+        {
+	        // Fetch one and one row
+	        while ($row=mysqli_fetch_row($result))
+	          {
+
+	          echo '<div id= ' ; echo $row[0]; echo'class="subtab">'; echo $row[0]; echo '</div>';
+	          }
+	        // Free result set
+	        mysqli_free_result($result);
+        }
+        $nb =$nb+1;
+    }
+
+
+
+
+
+      // $list_sensors = "SELECT Name_room 
+      //         FROM room 
+      //         WHERE id_home = 1"; 
+      // $results = mysqli_query($db,$list_sensors);
+      // echo '<ul class="navbar2">';
+      // if ($result=mysqli_query($db,$list_sensors))
+      //   {
+      //   // Fetch one and one row
+      //   while ($row=mysqli_fetch_row($result))
+      //     {
+      //     echo '<ul  onclick="opensubtab(';echo $row[0]; echo')">'; 
+
+      //     printf ($row[0]);
+      //     echo "</ul>";  
+
+      //     }
+      //   // Free result set
+      //   mysqli_free_result($result);
+      // }
+      // echo "</ul>";  
+
 
 
 
       // ____________________________________________ //
-      $nb = 1;
-      while ( $nb < 8) {      
-          $list_sensors = "SELECT sensor_name 
-              				FROM sensor 
-              				WHERE id_room = $nb"; 
-          $results2 = mysqli_query($db,$list_sensors);
-          $nb = $nb + 1;
+      // $nb = 1;
+      // while ( $nb < 8) {      
+      //     $list_sensors = "SELECT sensor_name 
+      //         				FROM sensor 
+      //         				WHERE id_room = $nb"; 
+      //     $results2 = mysqli_query($db,$list_sensors);
+      //     $nb = $nb + 1;
 
-          // cerate list of sensor to display
-          // to link to Room list previously created
+      //     // cerate list of sensor to display
+      //     // to link to Room list previously created
 
-          echo '<div id ="'; echo $nb; echo'" class="subtab">';
-          echo '</div>';
+      //     echo '<div id ="'; echo $nb; echo'" class="subtab">';
+      //     echo '</div>';
           
-      }
+      // }
 	  // ____________________________________________ //
 
-
-
-
-
-      mysqli_close($db);
-
-
-
+      //mysqli_close($db);
 
     ?>
     
