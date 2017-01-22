866656a57b37cbae140677034aa0f815eabf1053
antoine jeannette <antoine jeannette>
2017-01-22 12:48:10 +0100

    edit name dans maange house

--- managehouse.js
+++ managehouse.js
@@ -1,5 +1,6 @@
 function editsensorname(){
 	document.getElementById("sensorname").style.display = "block";
+	event.stopPropagation();
 }
 function editroomname(){
 	document.getElementById("roomname").style.display = "block";
--- managehouse.php
+++ managehouse.php
@@ -37,7 +37,7 @@
      	?>
 		<form id="addsensor" name="addsensor" method="post" accept-charset="utf-8">
 			<table cellspacing="15">
-				<tr><td>Sensor Name</td><td><input name ="sensor_name" placeholder ="Type sensor name" id="sensorinput" type="text" required></td></tr>
+				<tr><td>Sensor Name</td><td><input name ="sensor_name" placeholder ="Type sensor name" id="sensorinput" type="text" form="addsensor" required></td></tr>
 				<tr><td> Sensor Type </td>
 				<td><select name = "sensor_type" required form="addsensor">
 					<option value = "Default">Choose sensor</option>
@@ -103,15 +103,33 @@
 					<option>door</option>
 					<option>light</option>
 				</select></td></tr>
+				<tr><td>New Sensor Name</td><td><input type="text" name="renamesensor" form="editsensors" required></td></tr>
 				</table>
-				<button class="managehousebutton" onclick="editsensorname()">Edit Name</button>
-				<button class="managehousebutton">Delete</button>
+				<button class="managehousebutton" form="editsensors">Rename</button>
 			</form>
-			<form id="sensorname" name="sensorname">
+
+			<div id="titledeletesensors">Delete Sensors</div>
+
+			<form id="deletesensors" name="deletesensors">
 				<table cellspacing="15">
-				<tr><td>New Sensor Name</td><td><input type="text" form="sensorname" required></td></tr>
-				</table>
-				<button class="managehousebutton">Save</button>
+					<tr><td>Select Room</td>
+					<td><select form="deletesensors">
+					<option></option>
+					<option>living room</option>
+					<option>bedroom</option>
+					<option>etc...</option>
+					</select></td></tr>
+					<tr><td>Select Sensor</td>
+					<td><select required form="deletesensors">
+						<option></option>
+						<option>temperature</option>
+						<option>alarm</option>
+						<option>shutter</option>
+						<option>humidity</option>
+						<option>door</option>
+						<option>light</option>
+					</select></td></tr></table>
+				<button class="managehousebutton" form="deletesensors">Delete</button>
 			</form>
 	</div>
 
@@ -138,18 +156,18 @@
 
 	     ?>
 
-		<form id="addroom" method="post" accept-charset="utf-8">
+		<form id="addroom" name="addroom" method="post" accept-charset="utf-8">
 		<table cellspacing="15">
-		<tr><td>Room Name</td><td><input name = "add_room_name" id="roominput" type="text" required></td></tr>
+		<tr><td>Room Name</td><td><input name = "add_room_name" id="roominput" type="text" form="addroom" required></td></tr>
 		<tr><td>Room type</td>
-				<td><select name= "select_room_type" required form="editsensors">
+				<td><select name= "select_room_type" required form="addroom">
 				<option></option>
 				<option value = "1">kitchen</option>
 				<option value = "2">living room</option>
 				<option value = "3">bedroom</option>
-				<option value = "4">bathroom</option>
+				<option value = "4">bathroom</option></select></td></tr>
 		</table>
-		<button class="managehousebutton">Add</button>
+		<button class="managehousebutton" form="addroom">Add</button>
 		</form>
 
 		<?php
@@ -157,28 +175,34 @@
        ?>
 
 		<div id="titleeditrooms">Edit Rooms</div>
-
 			<form id="editrooms" name="editrooms" method="post" accept-charset="uft-8">
 				<table cellspacing="15">
 				<tr><td>Select Room</td>
-				<td><select form="editrooms">
+				<td><select form="editrooms" required>
 				<option></option>
 				<option>living room</option>
 				<option>bedroom</option>
 				<option>etc...</option>
 				</select></td></tr>
+				<tr><td>New Room Name</td><td><input type="text" name="renameroom" form="editrooms" required></td></tr>
 				</table>
-				<button class="managehousebutton" onclick="editroomname()">Edit Name</button>
-				<button class="managehousebutton">Delete</button>
+				<button class="managehousebutton" form="editrooms">Rename</button>
 			</form>
 
-			<form id="roomname" name="roomname" method="post" accept-charset="uft-8">
+			<div id="titledeleterooms">Delete Rooms</div>
+
+			<form id="deleterooms" name="deleterooms">
 				<table cellspacing="15">
-				<tr><td>Room Name</td><td><input type="text" form="roomname" required></td></tr>
-				</table>
-				<button class="managehousebutton">Save</button>
+					<tr><td>Select Room</td>
+					<td><select form="deleterooms" required>
+					<option></option>
+					<option>living room</option>
+					<option>bedroom</option>
+					<option>etc...</option>
+					</select></td></tr>
+					</table>
+				<button class="managehousebutton" form="deleterooms">Delete</button>
 			</form>
-
 	</div>
 
 </div>
